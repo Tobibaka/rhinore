e774831ff18c90527aae0cb8765202cd566fd5b6
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-10-05 01:39:37 +0530

    Add Escape and Sensor pages with initial layout and functionality

---
+++ src/app/admin/str/pages/sensor.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const sensor: React.FC = () => {
+  
+  
+  return (
+    <div>
+      <p className='text-gray-600'>
+        View your past donations and track your contribution history.
+      </p>
+    </div>
+  );
+};
+
+export default sensor;
