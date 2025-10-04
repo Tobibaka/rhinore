9e2e3e31dc93d1b06e7351332b4edfe290773996
Pritom Kalita <101991870+Tobibaka@users.noreply.github.com>
2025-10-05 01:38:46 +0530

    Add Approval, Connect, and Dashboard pages with initial layout

---
+++ src/app/admin/str/pages/connect.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const connect: React.FC = () => {
+  
+  
+  return (
+    <div>
+      <p className='text-gray-600'>
+        View your past donations and track your contribution history.
+      </p>
+    </div>
+  );
+};
+
+export default connect;
